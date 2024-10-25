2b06a1d5d2cec54dda35987ba561e6d03b02a9a4
Dimitri Dupuis-Latour <dupuislatour@me.com>
2024-10-25 20:17:46 +0200

    chore: cleanup, restructure, renames and polish

---
+++ ClientApp/AppSupport/ClientApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2024 Zama. All rights reserved.
+
+import SwiftUI
+
+@main
+struct ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ClientView()
+        }
+    }
+}
--- QuickLookExtension/SecureViews.swift
+++ QuickLookExtension/SecureViews.swift
@@ -1,47 +1,55 @@
 // Copyright © 2024 Zama. All rights reserved.
 
 import SwiftUI
+import Charts
 
 #Preview {
-    SecureView().gauge(value: 3, range: 1...5, title: "Sleep Quality", labels: ["Excellent", "Good", "Average", "Poor", "Awful"])
+    let vm = SecureView.ViewModel(data: .gauge(value: 3, range: 1...5, title: "Sleep Quality", labels: ["Excellent", "Good", "Average", "Poor", "Awful"]))
+    SecureView(viewModel: vm)
         .border(.red)
-        .privateDisplayRing()
         .border(.green)
 }
 
 struct SecureView: View {
     @ObservedObject var viewModel = ViewModel()
     
-    var body: some View {
-        switch viewModel.data {
-        case let .gauge(value, range, title, labels):
-            gauge(value: value, range: range, title: title, labels: labels)
-                .privateDisplayRing()
-
-        case .int(let int):
-            SecureTextView(value: int)
-
-        case .simpleChart(let array):
-            SecureChartView(values: array, kind: .lines)
-
-        case .none:
-            Color.red
+    final class ViewModel: ObservableObject {
+        @Published var data: Kind?
+        
+        init(data: SecureView.Kind? = nil) {
+            self.data = data
         }
     }
     
     enum Kind {
+        case text(value: Double)
         case gauge(value: Int, range: ClosedRange<Int>, title: String, labels: [String])
-        case int(Double)
         case simpleChart([Double])
     }
-    
-    final class ViewModel: ObservableObject {
-        @Published var data: Kind?
-    }
-}
 
-extension SecureView {
-    func gauge(value: Int, range: ClosedRange<Int>, title: String, labels: [String]) -> some View {
+    var body: some View {
+        Group {
+            switch viewModel.data {
+            case let .gauge(value, range, title, labels):
+                gauge(value: value, range: range, title: title, labels: labels)
+                
+            case .text(let value):
+                text(value: value)
+                
+            case .simpleChart(let values):
+                simpleChart(values)
+                
+            case .none:
+                Color.red
+            }
+        }.privateDisplayRing()
+    }
+
+    private func text(value: Double)  -> some View {
+        Text("\(value.formatted(.number.precision(.fractionLength(1))))")
+    }
+
+    private func gauge(value: Int, range: ClosedRange<Int>, title: String, labels: [String]) -> some View {
         VStack(spacing: 0) {
             Gauge(value: Double(value), in: Double(range.lowerBound)...Double(range.upperBound)) {
                 Text(title)
@@ -63,55 +71,20 @@ extension SecureView {
                 .font(.caption2)
         }
     }
-}
-
-struct SecureTextView: View {
-    let value: Double
     
-    var body: some View {
-        Text("\(value.formatted(.number.precision(.fractionLength(1))))")
-            .privateDisplayRing()
-    }
-}
-
-import Charts
-struct SecureChartView: View {
-    enum Kind { case lines, bars}
-    let values: [Double]
-    let kind: Kind
-    
-    var body: some View {
+    private func simpleChart(_ values: [Double])  -> some View {
         Chart {
             ForEach(Array(values.enumerated()), id: \.offset) { index, value in
-                switch kind {
-                case .lines:
-                    PointMark(
-                        x: .value("Index", index),
-                        y: .value("Value", value)
-                    )
-                    LineMark(
-                        x: .value("Index", index),
-                        y: .value("Value", value)
-                    )
-                case .bars:
-                    BarMark(
-                        x: .value("Index", index),
-                        y: .value("Value", value)
-                    )
-                }
+                PointMark(
+                    x: .value("Index", index),
+                    y: .value("Value", value)
+                )
+                LineMark(
+                    x: .value("Index", index),
+                    y: .value("Value", value)
+                )
             }
         }
         .chartXAxis(.hidden)
-//        .padding(4) // Chart draws some elements outside of its view
-        .privateDisplayRing()
     }
 }
-
-//#Preview {
-//    {
-//        return PrivateTextView(vm: .init())
-//    }()
-//    
-//    PrivateChartView(values: [6, 8, 7, 6, 6, 8, 7, 6, 9, 7], kind: .lines)
-//    PrivateChartView(values: [6, 8, 7, 6, 6, 8, 7, 6, 9, 7], kind: .bars)
-//}
