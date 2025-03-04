d0a7aab6476698528b7b2f39f1ae1032acdb88c7
Dimitri Dupuis-Latour <dupuislatour@me.com>
2025-03-04 19:24:52 +0100

    HealthApp > sleepTab coming to life

--- HealthApp/SleepTab.swift
+++ HealthApp/SleepTab.swift
@@ -7,19 +7,36 @@ import SwiftUI
 }
 
 struct SleepTab: View {
+    @StateObject var vm = ViewModel()
+    @Environment(\.scenePhase) var scenePhase
+
     var body: some View {
         NavigationStack {
             VStack(spacing: 16) {
-                OpenAppButton(.zamaDataVault(tab: .sleep)) {
-                    Text("Import Encrypted Data")
-                }
-
-                AsyncButton("Select Encrypted Data") {
-                    print("Show date picker")
+                if vm.samplesAvailable {
+                    AsyncButton("Select Encrypted Data") {
+                        vm.selectSample()
+                    }
+                } else {
+                    OpenAppButton(.zamaDataVault(tab: .sleep)) {
+                        Text("Import Encrypted Data")
+                    }
                 }
 
                 CustomBox("Sleep Phase") {
-                    Text("No data found")
+                    if let url = vm.sampleSelected {
+                        FilePreview(url: url)
+                        
+                        Text("""
+                            **Awake**: Often brief and unnoticed.
+                            **REM**: Dreaming stage, crucial for memory and emotions.
+                            **Core**: Light sleep, prepares the body for deeper stages.
+                            **Deep**: Restorative sleep, vital for physical recovery and growth.
+                            """)
+
+                    } else {
+                        Text("No data found")
+                    }
                 }
                 
                 CustomBox("Sleep Quality") {
@@ -32,6 +49,35 @@ struct SleepTab: View {
             .navigationTitle("Sleep Analysis")
             .buttonStyle(.custom)
             .background(Color.zamaYellowLight)
+            .onChange(of: scenePhase) { _, newPhase in
+                if newPhase == .active {
+                    vm.onAppActive()
+                }
+            }
+        }
+    }
+}
+
+extension SleepTab {
+    @MainActor final class ViewModel: ObservableObject {
+        @Published var samplesAvailable: Bool
+        @Published var sampleSelected: URL?
+                
+        init() {
+            self.samplesAvailable = false
+            self.sampleSelected = nil
+        }
+        
+        func onAppActive() {
+            Task {
+                let foundSamples = await Storage.read(.sleepList)
+                self.samplesAvailable = foundSamples != nil
+                self.sampleSelected = nil
+            }
+        }
+        
+        func selectSample() {
+            self.sampleSelected = Storage.url(for: .sleepList)
         }
     }
 }
