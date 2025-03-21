44478c4b3d29a7a6aaea2938c5956caf2dad3eec
kcelia <celia.kherfallah@zama.ai>
2025-03-21 18:06:04 +0100

    chore: import weight test

--- Server/tasks/weight_stats/src/lib.rs
+++ Server/tasks/weight_stats/src/lib.rs
@@ -44,6 +44,18 @@ fn serialize_compressed_key(compressed_server_key: &CompressedServerKey, path: &
     fs::write(path_ct, &serialized_ct).expect("Failed to write serialized key to file.");
 }
 
+fn serialize_client_key(compressed_server_key: &ClientKey, path: &str) {
+    // Serializes a compressed server key and saves it to a file.
+    let serialized_ct = bincode::serialize(compressed_server_key).expect("Failed to serialize key.");
+    let path_ct = Path::new(path);
+    fs::write(path_ct, &serialized_ct).expect("Failed to write serialized key to file.");
+}
+
+fn deserialize_client_key(path: &str) -> ClientKey {
+    let path_ct = Path::new(path);
+    let serialized_ct = fs::read(path_ct).expect("Failed to read serialized key file.");
+    bincode::deserialize(&serialized_ct).expect("Failed to deserialize client key.")
+}
 
 fn serialize_compactciphertextlist(encrypted_data: &CompactCiphertextList, path: &str) {
     // Serializes an encrypted `CompactCiphertextList` and saves it to a file.
@@ -73,22 +85,18 @@ fn min(data: &[u16]) -> u16 {
 
 
 #[pyfunction]
-pub fn compute_stats() -> PyResult<u8> {
+pub fn generate_files(clear_data: Vec<f64>, uid: String) -> PyResult<u8> {
 
     let current_dir = env::current_dir().unwrap();
     println!("Starting weight stats test... from directory: {}", current_dir.display());
-    let uid = "test_weight_stats";
 
     let sk_path = format!("./project/uploaded_files/{}.serverKey", uid);
+    let ck_path = format!("./project/uploaded_files/{}.clientKey", uid);
     let input_path = format!("./project/uploaded_files/{}.weight_stats.input.fheencrypted", uid);
-    let output_avg_path = format!("./project/uploaded_files/{}.outputAvg.weight_stats.fheencrypted", uid);
-    let output_min_path = format!("./project/uploaded_files/{}.outputMin.weight_stats.fheencrypted", uid);
-    let output_max_path = format!("./project/uploaded_files/{}.outputMax.weight_stats.fheencrypted", uid);
+
     println!("sk_path    : {}", sk_path);
+    println!("ck_path    : {}", ck_path);
     println!("input_path : {}", input_path);
-    println!("output_avg_path: {}", output_avg_path);
-    println!("output_min_path: {}", output_min_path);
-    println!("output_max_path: {}", output_max_path);
 
     let config = ConfigBuilder::default().build();
     let client_key = ClientKey::generate(config);
@@ -102,9 +110,7 @@ pub fn compute_stats() -> PyResult<u8> {
     println!("Server key (decompressed size: {} bytes)", decompressed_size);
 
     set_server_key(sks.clone());
-    serialize_compressed_key(&compressed_server_key, &sk_path);
 
-    let clear_data: Vec<f64> = vec![68.0, 65.0, 69.0, 70.0, 70.5];
     println!("Original weight : {:?}", clear_data);
 
     let clear_data: Vec<u16> = clear_data.into_iter().map(|w| (w * 10.0) as u16).collect();
@@ -118,19 +124,45 @@ pub fn compute_stats() -> PyResult<u8> {
     }
 
     let compact_list = builder.build();
-    
+
+    serialize_compressed_key(&compressed_server_key, &sk_path);
+    serialize_client_key(&client_key, &ck_path);
     serialize_compactciphertextlist(&compact_list, &input_path);
 
+    
+    Ok(1)
+}
+
+
+#[pyfunction]
+pub fn run(uid: String) -> PyResult<u8> {
     // Call main.rs
     let _status = Command::new("cargo")
         .args(&[
-            "run", "--bin", "weight_stats", "--manifest-path", "tasks/weight_stats/Cargo.toml", "--", uid
+            "run", "--bin", "weight_stats", "--manifest-path", "tasks/weight_stats/Cargo.toml", "--", &uid
         ])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status()
         .expect("Failed to test weight_stats use-case.");
         
+    Ok(1)
+}
+    
+#[pyfunction]
+pub fn decrypt_weight_stats(uid: String) -> PyResult<(u16, u16, u16)> {
+
+    let ck_path = format!("./project/uploaded_files/{}.clientKey", uid);
+    let output_avg_path = format!("./project/uploaded_files/{}.outputAvg.weight_stats.fheencrypted", uid);
+    let output_min_path = format!("./project/uploaded_files/{}.outputMin.weight_stats.fheencrypted", uid);
+    let output_max_path = format!("./project/uploaded_files/{}.outputMax.weight_stats.fheencrypted", uid);
+    
+    println!("output_avg_path: {}", output_avg_path);
+    println!("output_min_path: {}", output_min_path);
+    println!("output_max_path: {}", output_max_path);
+
+    let deserialize_ck = deserialize_client_key(&ck_path);
+
     // Retrive the encrypted result
     let avg_encrypted_output = deserialize_fheuint16(&output_avg_path);
     let file_size = fs::metadata(&output_avg_path).unwrap().len();
@@ -145,49 +177,51 @@ pub fn compute_stats() -> PyResult<u8> {
     println!("Max weight retieved at: {} (size: {})", output_max_path, file_size);
 
     // Decrypt the output
-    let decrypted_avg_weight: u16 = avg_encrypted_output.decrypt(&client_key);
+    let decrypted_avg_weight: u16 = avg_encrypted_output.decrypt(&deserialize_ck);
     println!("Avg weight: {}", decrypted_avg_weight);
-    let decrypted_min_weight: u16 = min_encrypted_output.decrypt(&client_key);
+    let decrypted_min_weight: u16 = min_encrypted_output.decrypt(&deserialize_ck);
     println!("Min weight: {}", decrypted_min_weight);
-    let decrypted_max_weight: u16 = max_encrypted_output.decrypt(&client_key);
+    let decrypted_max_weight: u16 = max_encrypted_output.decrypt(&deserialize_ck);
     println!("Max weight: {}", decrypted_max_weight);
 
-    assert!(mean(&clear_data) == decrypted_avg_weight);
-    assert!(max(&clear_data) == decrypted_max_weight);
-    assert!(min(&clear_data) == decrypted_min_weight);
-
-    let clear_avg = mean(&clear_data);
-    let clear_max = max(&clear_data);
-    let clear_min = min(&clear_data);
-    
-    assert_eq!(
-        clear_avg,
-        decrypted_avg_weight,
-        "Error: computed average weight does not match the expected value. Expected: {}, got: {}",
-        decrypted_avg_weight,
-        clear_avg
-    );
-    assert_eq!(
-        clear_max,
-        decrypted_max_weight,
-        "Error: computed maximum weight does not match the expected value. Expected: {}, got: {}",
-        decrypted_max_weight,
-        clear_max
-    );
-    assert_eq!(
-        clear_min,
-        decrypted_min_weight,
-        "Error: computed minimum weight does not match the expected value. Expected: {}, got: {}",
-        decrypted_min_weight,
-        clear_min
-    );
-    
-    Ok(1)
+    Ok((decrypted_avg_weight, decrypted_min_weight, decrypted_max_weight))
 }
-
+    
 
 #[pymodule]
 fn weight_stats(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(compute_stats, m)?)?;
+    m.add_function(wrap_pyfunction!(generate_files, m)?)?;
+    m.add_function(wrap_pyfunction!(run, m)?)?;
+    m.add_function(wrap_pyfunction!(decrypt_weight_stats, m)?)?;
     Ok(())
 }
+
+    // assert!(mean(&clear_data) == decrypted_avg_weight);
+    // assert!(max(&clear_data) == decrypted_max_weight);
+    // assert!(min(&clear_data) == decrypted_min_weight);
+
+    // let clear_avg = mean(&clear_data);
+    // let clear_max = max(&clear_data);
+    // let clear_min = min(&clear_data);
+    
+    // assert_eq!(
+    //     clear_avg,
+    //     decrypted_avg_weight,
+    //     "Error: computed average weight does not match the expected value. Expected: {}, got: {}",
+    //     decrypted_avg_weight,
+    //     clear_avg
+    // );
+    // assert_eq!(
+    //     clear_max,
+    //     decrypted_max_weight,
+    //     "Error: computed maximum weight does not match the expected value. Expected: {}, got: {}",
+    //     decrypted_max_weight,
+    //     clear_max
+    // );
+    // assert_eq!(
+    //     clear_min,
+    //     decrypted_min_weight,
+    //     "Error: computed minimum weight does not match the expected value. Expected: {}, got: {}",
+    //     decrypted_min_weight,
+    //     clear_min
+    // );
--- Server/tests/test_weight_stats.py
+++ Server/tests/test_weight_stats.py
@@ -1,10 +1,47 @@
+import os
 import time
+
+import numpy as np
+
 import weight_stats
 
 def test_weight_stats():
     print("\nweight_stats module loaded, calling compute_stats()...")
+    
+    weights_list = [68.0, 65.0, 69.0, 70.0, 70.5]
+    
+    uid = "test_weight_stats"
+    
+    ck_path = f"./project/uploaded_files/{uid}.clientKey"
+    sk_path = f"./project/uploaded_files/{uid}.serverKey"
+
+    input_path = f"./project/uploaded_files/{uid}.weight_stats.input.fheencrypted"
+    
+    output_avg_path = f"./project/uploaded_files/{uid}.outputAvg.weight_stats.fheencrypted"
+    output_min_path = f"./project/uploaded_files/{uid}.outputMin.weight_stats.fheencrypted"
+    output_max_path = f"./project/uploaded_files/{uid}.outputMax.weight_stats.fheencrypted"
+    
     start_time = time.time()
-    weight_stats.compute_stats()
+    
+    weight_stats.generate_files(weights_list, uid)
+
+    assert os.path.exists(sk_path), f"Missing file: {sk_path=}"
+    assert os.path.exists(ck_path), f"Missing file: {ck_path=}"
+    assert os.path.exists(input_path), f"Missing file: {input_path=}"
+    
+    weight_stats.run(uid)
+
+    assert os.path.exists(output_avg_path), f"Missing file: {output_avg_path=}"
+    assert os.path.exists(output_min_path), f"Missing file: {output_min_path=}"
+    assert os.path.exists(output_max_path), f"Missing file: {output_max_path=}"
+
+    # Decrypt and check results
+    decrypted_avg, decrypted_min, decrypted_max = weight_stats.decrypt_weight_stats(uid)
+
+    assert np.mean(weights_list) * 10 == decrypted_avg, f"Expected avg: {np.mean(weights_list)}, got: {decrypted_avg}"
+    assert np.min(weights_list) * 10 == decrypted_min, f"Expected min: {np.min(weights_list)}, got: {decrypted_min}"
+    assert np.max(weights_list) * 10 == decrypted_max, f"Expected max: {np.max(weights_list)}, got: {decrypted_max}"
+
     end_time = time.time() - start_time
     print(f"Test execution time: {end_time:.2f} seconds")
 
