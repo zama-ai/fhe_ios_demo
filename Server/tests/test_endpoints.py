ebdaebf0800373ea3544a837c7274d038a04ca1b
jfrery <jordan.frery@zama.ai>
2025-05-14 15:53:49 +0200

    test: add stress tests

--- Server/tests/test_endpoints.py
+++ Server/tests/test_endpoints.py
@@ -6,8 +6,6 @@ import pytest
 from utils import *
 
 
-ID_PATTERN = re.compile(r"^[a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$")
-
 # In rare cases, the status may be unknown/complete if the task has not yet started or has finished.
 # Thus, we restart the test if it fails.
 @pytest.mark.flaky()
--- Server/tests/utils.py
+++ Server/tests/utils.py
@@ -9,6 +9,8 @@ import subprocess
 
 from pathlib import Path
 
+ID_PATTERN = re.compile(r"^[a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$")
+
 # Chargement du fichier YAML
 with open("tasks.yaml", encoding="utf8", mode="r") as file:
     TASK_CONFIG = yaml.safe_load(file)
